e11c1a04f4d7d6d00665bb56e17bfba032297020
Daniel Herrero García <daniel.herrero@kaleidos.net>
2022-12-21 11:49:26 +0100

    refactor class methods

--- __init__.py
+++ __init__.py
@@ -1,66 +1,74 @@
-import json
 import unidecode
 from mycroft import MycroftSkill, intent_file_handler
-from lnetatmo import ClientAuth, WeatherStationData
-
-
-weather_data = None
-username = None
-password = None
-client_id = None
-client_secret = None
+from lnetatmo import ClientAuth, WeatherStationData, AuthFailure
 
 
 class NetatmoWeather(MycroftSkill):
+    weather_data = None
+    username = None
+    password = None
+    client_id = None
+    client_secret = None
+
     def __init__(self):
         MycroftSkill.__init__(self)
-        _load_setting_variables(self)
-        _read_netatmo_weather(self)
+        self.load_setting_variables()
+        self.read_netatmo_weather()
+
+    def initialize(self):
+        self.settings_change_callback = self.on_settings_changed
+        self.on_settings_changed()
+
+    def on_settings_changed(self):
+        self.load_setting_variables()
+        self.read_netatmo_weather()
 
     @intent_file_handler('weather_netatmo.intent')
     def handle_weather_netatmo(self, message):
-        modules_weather_data = _parse_netatmo_weather(weather_data.rawData)
-        self.log.info(f"Weather json  > {modules_weather_data.keys()}")
-
-        module = self.get_response("request_module").lower()
-        self.log.info(f"Input module  > {module}")
-
-        if module not in modules_weather_data.keys():
-            self.speak_dialog('error_module_missing', {'module': module})
+        if not self.weather_data:
+            self.speak_dialog('error_auth')
         else:
-            module_weather = modules_weather_data[module]
-            self.log.info(f"Temperature  > {module_weather['Temperature']}")
-            self.speak_dialog('weather_netatmo', data={'module': module, 'temp': module_weather["Temperature"]})
+            modules_weather_data = parse_netatmo_weather(self.weather_data.rawData)
+            self.log.info(f"Weather modules  > {list(modules_weather_data.keys())}")
+
+            module = self.get_response("request_module").lower()
+            self.log.info(f"Input module  > {module}")
+
+            if module not in modules_weather_data.keys():
+                self.speak_dialog('error_module_missing', {'module': module})
+            else:
+                module_weather = modules_weather_data[module]
+                self.log.info(f"Temperature  > {module_weather['Temperature']}")
+                self.speak_dialog('weather_netatmo', data={'module': module, 'temp': module_weather["Temperature"]})
+
+    def load_setting_variables(self):
+        self.username = self.settings.get('username')
+        self.password = self.settings.get('password')
+        self.client_id = self.settings.get('client_id')
+        self.client_secret = self.settings.get('client_secret')
+
+    def read_netatmo_weather(self):
+        try:
+            authorization = ClientAuth(
+                clientId=self.client_id,
+                clientSecret=self.client_secret,
+                username=self.username,
+                password=self.password
+            )
+            self.weather_data = WeatherStationData(authorization)
+        except AuthFailure:
+            self.log.error(f"Authentication error. Please review the skill configuration.")
 
 
 def create_skill():
     return NetatmoWeather()
 
 
-def _load_setting_variables(self):
-    global username, password, client_id, client_secret
-    username = self.settings.get('username')
-    password = self.settings.get('password')
-    client_id = self.settings.get('client_id')
-    client_secret = self.settings.get('client_secret')
-
-
-def _read_netatmo_weather(self):
-    authorization = ClientAuth(
-        clientId=client_id,
-        clientSecret=client_secret,
-        username=username,
-        password=password)
-
-    global weather_data
-    weather_data = WeatherStationData(authorization)
-
-
-def _normalize(text: str) -> str:
+def normalizeText(text: str) -> str:
     return unidecode.unidecode(text.lower())
 
 
-def _parse_netatmo_weather(stations_weather_data):
+def parse_netatmo_weather(stations_weather_data):
     if len(stations_weather_data) == 0:
         return {}
     else:
@@ -68,9 +76,9 @@ def _parse_netatmo_weather(stations_weather_data):
         station_data = stations_weather_data[0]
 
     weather_json = dict()
-    weather_json[_normalize(station_data["module_name"])] = station_data["dashboard_data"]
+    weather_json[normalizeText(station_data["module_name"])] = station_data["dashboard_data"]
 
     for module_data in station_data["modules"]:
-        weather_json[_normalize(module_data["module_name"])] = module_data["dashboard_data"]
+        weather_json[normalizeText(module_data["module_name"])] = module_data["dashboard_data"]
 
     return weather_json
